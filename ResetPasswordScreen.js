6b90103167144e752bc47916eb49c37bb63c43c6
SadafAsad <sadaf98x@gmail.com>
2022-10-02 18:47:39 -0400

    Worked on ResetPasswordScreen

--- ResetPasswordScreen.js
+++ ResetPasswordScreen.js
@@ -1,14 +1,72 @@
-import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, Pressable, View, TextInput } from 'react-native';
+import { RadioButton } from 'react-native-paper';
+import { useState } from "react";
+import { StackActions } from '@react-navigation/native';
 
-const ResetPasswordScreen = () => {
+const ResetPasswordScreen = ({navigation}) => {
+    const [checked, setChecked] = useState('');
+
     return (
-        <SafeAreaView>
-            <Text>Reset Password Screen</Text>
+        <SafeAreaView style={{backgroundColor:'#fff', flex:1, justifyContent:'space-between'}}>
+            <View>
+                <Text style={{fontSize:20, margin:22}}>We need to verify your identity</Text>
+
+                <RadioButton.Group onValueChange={checked => setChecked(checked)} value={checked}>
+                    <RadioButton.Item label="Email" value="Email" />
+                    <RadioButton.Item label="Text" value="Text" />
+                </RadioButton.Group>
+
+                { (checked==='Email') && (
+                    <TextInput 
+                        style={styles.input}
+                        placeholder="Enter email"
+                        keyboardType="email-address"
+                        autoCapitalize="none"
+                    />
+                )}
+
+                { (checked==='Text') && (
+                    <TextInput 
+                        style={styles.input}
+                        placeholder="Enter phone number"
+                        keyboardType="phone-pad"
+                        autoCapitalize="none"
+                    />
+                )}
+            </View>
+
+            <View style={{marginTop:20}}>
+                <Pressable onPress={ () => {
+                    navigation.dispatch(StackActions.replace('TabsNavigator'))
+                }}>
+                    <Text style={styles.verificationPressable}>Send Verification Code</Text>
+                </Pressable>
+            </View>
         </SafeAreaView>
     );
 }
   
 const styles = StyleSheet.create({
+    verificationPressable: {
+        alignSelf: 'center',
+        textAlign: 'center',
+        backgroundColor: '#335C67',
+        color: '#ffffff',
+        marginLeft: 22,
+        marginRight: 22,
+        marginTop: 22,
+        fontSize: 18,
+        padding: 15,
+        width: 350,
+    },
+    input: {
+        alignSelf: 'center',
+        height: 40,
+        width: 350,
+        borderWidth: 1,
+        padding: 10,
+        borderColor: '#808080',
+    },
 });
 
 export default ResetPasswordScreen;
