c7d011da6f665a7e64d41343568de3e1cb1eab73
SadafAsad <sadaf98x@gmail.com>
2022-10-11 22:50:06 -0400

    Addeess Info Screen --not complete

--- AddressInfo.js
+++ AddressInfo.js
@@ -1,9 +1,10 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Pressable, TextInput } from 'react-native';
 import { ProgressSteps, ProgressStep } from 'react-native-progress-steps';
+import { StackActions } from '@react-navigation/native';
 
-const AddressInfo = () => {
+const AddressInfo = ({navigation}) => {
     return (
-        <SafeAreaView>
+        <SafeAreaView style={{flex:1, alignSelf:'center'}}> 
             <View style={{height:120}}> 
                 <ProgressSteps
                     borderWidth={3}
@@ -22,11 +23,81 @@ const AddressInfo = () => {
                     <ProgressStep label="Address" removeBtnRow={true}/>
                 </ProgressSteps>
             </View>
+            <Text style={styles.screentitle}>Address Info</Text>
+            <Text style={styles.descTxt}>Complete your profile information. It will only take a few minutes.</Text>
+            <Text style={styles.titleTxt}>Address</Text>
+            <TextInput 
+                style={styles.input}
+                placeholder=""
+                keyboardType="email-address"
+                autoCapitalize="none"
+            />
+            <Text style={styles.titleTxt}>Address line 2</Text>
+            <TextInput 
+                style={styles.input}
+                placeholder=""
+                keyboardType="email-address"
+                autoCapitalize="none"
+            />
+            <Text style={styles.titleTxt}>City</Text>
+            <TextInput 
+                style={styles.input}
+                placeholder=""
+                keyboardType="email-address"
+                autoCapitalize="none"
+            />
+            <Text style={styles.titleTxt}>Country</Text>
+            <Text style={styles.titleTxt}>Province / State</Text>
+            <Text style={styles.titleTxt}>Postal Code</Text>
+            <Pressable onPress={ () => {
+                navigation.dispatch(StackActions.replace('TabsNavigator'))
+            }}>
+                <Text style={styles.PressableStyle}>Create Account</Text>
+            </Pressable>
         </SafeAreaView>
     );
 }
   
 const styles = StyleSheet.create({
+    input: {
+        alignSelf: 'center',
+        height: 40,
+        width: 350,
+        borderWidth: 1,
+        padding: 10,
+        borderColor: '#808080',
+    },
+    PressableStyle: {
+        alignSelf: 'center',
+        textAlign: 'center',
+        backgroundColor: '#335C67',
+        color: '#ffffff',
+        marginLeft: 22,
+        marginRight: 22,
+        marginTop: 22,
+        fontSize: 18,
+        padding: 15,
+        width: 350,
+    },
+    titleTxt: {
+        marginTop:20, 
+        marginBottom:5, 
+        marginLeft:22, 
+        marginRight:22
+    },
+    descTxt: {
+        fontSize:15, 
+        color:'#808080', 
+        marginTop:5, 
+        marginLeft:22, 
+        marginRight:22
+    },
+    screentitle: {
+        fontWeight:'bold', 
+        fontSize:30, 
+        marginLeft:22, 
+        marginRight:22
+    }
 });
 
 export default AddressInfo;
