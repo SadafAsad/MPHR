0dd349e8c9db11138c0934709207131f368ae538
Jiisgone <chaki.terrier@gmail.com>
2022-10-20 02:08:47 -0400

    Changed vet icon, integrated firestore database for vet list.

--- VetsTabScreen.js
+++ VetsTabScreen.js
@@ -2,15 +2,42 @@ import React, { useState, useEffect } from 'react';
 import { SafeAreaView, StyleSheet, Text, FlatList, Pressable, View, Image, TextInput,} from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 
+import { db } from "./FirebaseApp"
+import { collection, doc, getDocs } from "firebase/firestore";
+
 const VetsTabScreen = () => {
     const [search, setSearch] = useState('');
     const [filteredDataSource, setFilteredDataSource] = useState([]);
     const [masterDataSource, setMasterDataSource] = useState([]);
 
     useEffect(() => {
-            setFilteredDataSource(vets);
-            setMasterDataSource(vets);
+        getVetList()
+            // setFilteredDataSource(vets);
+            // setMasterDataSource(vets);
     }, []);
+
+    const getVetList = async () => {
+        try {
+            let querySnapshot = await getDocs(collection(db, "vet_list"));
+
+            let documents = querySnapshot.docs
+
+            // FOR TESTING PURPOSES
+            // for (let i = 0; i < documents.length; i++) {
+            //     const currDocument = documents[i]
+            //     console.log(`ID: ${currDocument.id}`)
+            //     console.log(currDocument.data())
+            //     console.log("------")
+            // }
+
+            setFilteredDataSource(documents);
+            setMasterDataSource(documents);
+
+        } catch (err) {
+            console.log(`${documents.id}`)    
+            console.log(`${err.message}`)        
+        }
+    }
     
     const vets = [
         {id: 1, name: "Merck Animal Health", address: "16750 route Transcanadienne\nKirkland, Quebec H9H 4M7"},
@@ -25,14 +52,17 @@ const VetsTabScreen = () => {
         }>
             <View style={styles.vet}>
                 <View style={{flexDirection:'row', justifyContent:'space-between', alignItems:'center'}}>
-                    <Image source={require('./assets/pet-icon-2.png')} style={styles.img}/>
+                    <Image source={require('./assets/physical-examination-1.png')} style={styles.img}/>
                     <View>
                         <View style={{flexDirection:'column', marginLeft:20, alignItems:'baseline'}}>
-                            <Text style={{fontSize:18, fontWeight:'bold'}}>{item.name}</Text>
-                            <Text style={{fontSize:14}}>{item.address}</Text>
+                            <Text style={{fontSize:18, fontWeight:'bold'}}>{item.data().name}</Text>
+                            <Text style={{fontSize:14}}>{item.data().street_address}</Text>
+                            <Text style={{fontSize:14}}>{item.data().city_address}</Text>
+                            <Text style={{fontSize:14}}>+1 {item.data().phone_number}</Text>
                         </View> 
                     </View>
                 </View>
+                {/* arrow right icon */}
                 <AntDesign name="right" size={20} color='#335C67' style={{marginRight:22}}/>
             </View>
         </Pressable>
@@ -64,7 +94,7 @@ const VetsTabScreen = () => {
 
     return (
         <SafeAreaView style={styles.container}>
-            <Text>Vets Tab Screen!</Text>
+            {/* <Text>Vets Tab Screen!</Text>    */}
             <TextInput
                 style={styles.textInputStyle}
                 onChangeText={(text) => searchFilterFunction(text)}
@@ -105,6 +135,8 @@ const styles = StyleSheet.create({
     },
     textInputStyle: {
       height: 40,
+      width: '90%',
+      alignSelf: 'center',
       borderWidth: 1,
       paddingLeft: 20,
       margin: 5,
