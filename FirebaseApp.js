b926088ba1ca90bfcb3070a835c24abdb98e999e
SadafAsad <34832185+SadafAsad@users.noreply.github.com>
2022-11-10 09:28:07 -0500

    firebase storage setup

--- FirebaseApp.js
+++ FirebaseApp.js
@@ -5,12 +5,16 @@ import firebaseConfig from "./config/firebase-keys"
 import { initializeApp } from 'firebase/app';
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
  
 // 2. instantiate the firebase app
 // 3. the "export" keyword enables the firebaseApp variable to be accessible outside this file
 export const firebaseApp = initializeApp(firebaseConfig);
  
 // 4. @TODO: instantiate any other firebase services here
-export const auth = getAuth(firebaseApp)
+export const auth = getAuth(firebaseApp);
 
-export const db = getFirestore(firebaseApp)
+export const db = getFirestore(firebaseApp);
+
+// Initialize Cloud Storage and get a reference to the service
+export const storage = getStorage(app);
